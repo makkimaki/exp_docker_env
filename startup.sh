fae7acaf000a1aa64430092080c67eafbaeb2110
makkimaki <shellingford-93.4.2@hotmail.co.jp>
2021-12-09 05:05:57 +0000

    Add: container startup command (complete!)
    これでcondaパス、sshdなど完成した

--- startup.sh
+++ startup.sh
@@ -3,11 +3,10 @@
 . ~/.bashrc
 
 echo "startup: start jupyterlab"
-# nohup jupyter lab --allow-root --ip=0.0.0.0 --no-browser &
 
-# conda virtual env setup
-# /opt/miniconda3/condabin/conda create -y --name conda39 python=3.9.1 pip 
 # for Dockerfile CMD instruction
+/opt/miniconda3/condabin/conda create -y python=3.9.1 pip --name conda39
+service ssh restart 
 /bin/bash 
 echo "startup: start sshd.."
 /usr/sbin/sshd -D
