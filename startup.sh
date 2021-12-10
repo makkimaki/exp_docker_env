771396f5a183afc6b390c138f7e73e38f545c228
makkimaki <shellingford-93.4.2@hotmail.co.jp>
2021-12-10 03:28:06 +0000

    Modify: virtual env library building
    仮想環境へのライブラリインストールを実施

--- startup.sh
+++ startup.sh
@@ -6,7 +6,11 @@ echo "startup: start jupyterlab"
 
 # for Dockerfile CMD instruction
 /opt/miniconda3/condabin/conda create -y python=3.9.1 pip --name conda39
+source activate conda39
 service ssh restart 
+pip install -r requirements.txt
+pip install --no-binary :all: nmslib
+
 /bin/bash 
 echo "startup: start sshd.."
 /usr/sbin/sshd -D
